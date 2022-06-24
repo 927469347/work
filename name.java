4529a5d32997a326c50563af0bfa0f5e9721dfe2
zzh <927469347@qq.com>
2022-06-24 16:09:54 +0800

    name1

---
+++ name.java
@@ -0,0 +1,9 @@
+import javax.swing.text.StyledEditorKit;
+
+public class name {
+    public static void main(String[] args) {
+        System.out.println("爱你");
+        System.out.println("爱你1");
+        System.out.println("爱你2");
+    }
+}
